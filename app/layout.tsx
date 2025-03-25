8b17ed8cf5e5f41e9dea42357ba2d8304e356036
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-26 00:31:10 +0530

    👀 realtime(analytics): add Vercel Analytics component to layout for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -76,6 +77,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster />
+        <Analytics/>
         {children}
       </body>
     </html>
