6256826edaab2a89301a4b82dbbef94d2909528e
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-26 00:31:32 +0530

    🧹 linti(fix): adjust formatting of Geist font declaration for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/react"
 
-const geistSans = Geist({
+const geistSans = Geist({ 
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
